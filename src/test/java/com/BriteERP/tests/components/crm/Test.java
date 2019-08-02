437f428d12dbc4e570aa7e0e18bde5b4deaef0cb
mehmetdurkan <mehmet_ durkan@icloud.com>
2019-08-02 13:14:20 -0500

    Group 28 Brite ERP V-5 We have Develop Branch now

--- src/test/java/com/BriteERP/tests/components/crm/Test.java
+++ src/test/java/com/BriteERP/tests/components/crm/Test.java
@@ -5,5 +5,25 @@ import com.BriteERP.utilities.TestBase;
 public class Test extends TestBase {
 
 // Wake up Mehmet Abi :)
+    // Branches are active from now on.
+    //nobody will push to master until we agree
+    //master is the one we will push everything every sprint
+    //develop is the one you will try to merge and handle conflicts
+    //Tester_Your_Name will be the branch you will work on and check out for your work
+    //So, we will have
+            //
+//    1.Master
+//    2.Develop
+//    3.Testers names
+//        a. Mehmet
+//        b. Rozi
+//        c. Francesqua
+//        d. Tugba
+//        e. Goksel
+//    total 7 branches
+//
+//
+
+
 
 }
--- src/test/java/com/BriteERP/tests/components/crm/Test2.java
+++ src/test/java/com/BriteERP/tests/components/crm/Test2.java
@@ -2,10 +2,8 @@ package com.BriteERP.tests.components.crm;
 
 public class Test2 {
 
-    // Mehmet Abi created by Goksel
 
-
-
+    
     public void test(){
 
     }
