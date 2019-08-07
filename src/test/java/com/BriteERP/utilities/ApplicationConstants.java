fa9fc0979713402074d81d2ce27c52a22643761c
TugbaKursun <busrazra814@gmail.com>
2019-08-06 22:21:22 -0500

    create a branch

---
+++ src/test/java/com/BriteERP/utilities/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.BriteERP.utilities;
+
+public class ApplicationConstants {
+
+    public static final String SERACH_MORE_EMPLOYEE_NAME="Ashley Presley";
+    public static final String BROWSERCHROME="chrome";
+
+}
